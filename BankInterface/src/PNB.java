07f3917f9cff46e8db7badedc5b835a81cea2c6d
Zack Bristor <zbristor@gmail.com>
2017-06-01 14:34:11 -0400

    JavaBootCamp Assignment

---
+++ BankInterface/src/PNB.java
@@ -0,0 +1,9 @@
+class PNB implements Bank{  
+public float rateOfInterest(){return 9.7f;}
+
+@Override
+public String justoutput() {
+	// TODO Auto-generated method stub
+	return null;
+}  
+}  
---
+++ BankInterface/src/TestInterface2.java
@@ -0,0 +1,9 @@
+class TestInterface2{  
+public static void main(String[] args){  
+Bank b=new SBI(); 
+SBI test = new SBI();
+System.out.println("ROI: "+b.rateOfInterest() );
+System.out.println(b.justoutput());
+System.out.println(test.justout());
+
+}}  
